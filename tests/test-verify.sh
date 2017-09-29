f5dd3c093d7f70f96a278760d31a99e2b3fe3b89
Adam Dyson <contact@adamdyson.com.au>
2017-09-29 20:16:26 +1000

    Simplified testing role using Travis CI and docker containers.

---
+++ tests/test-verify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Verify the installed location.
+docker exec --tty ${container_id} env TERM=xterm which node
+
+# Verify the installed version.
+docker exec --tty ${container_id} env TERM=xterm node -v
+
+# Verify the installed global packages.
+docker exec --tty ${container_id} env TERM=xterm bash --login -c "npm root -g
