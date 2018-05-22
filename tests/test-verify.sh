da9425b7fab5e948349136ccdfc77d725b699890
Adam Dyson <adam.dyson@outlook.com>
2018-05-23 01:01:55 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
 # Verify the installed location.
 docker exec --tty ${container_id} env TERM=xterm which node
-
 # Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm node -v
-
 # Verify the global NPM location.
 docker exec --tty ${container_id} env TERM=xterm bash --login -c "npm root -g"
-
 # Verify the modules installed globally.
 docker exec --tty ${container_id} env TERM=xterm bash --login -c "npm list -g --depth=0"
